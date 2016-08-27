803c40dc36e4cfd86bacff083350e19cc260b2d9
ShinYN <go1323@gmail.com>
2016-08-28 06:53:20 +0900

    Encrypt 알고스팟 업데이트

--- Algorithm/Algospot/ENCRYPT.py
+++ Algorithm/Algospot/ENCRYPT.py
@@ -1,14 +1,8 @@
-def SplitStringToArraryByLength(inputString, substringLength):
-    return [inputString[i:i+substringLength] for i in range(0, len(inputString), substringLength)]
-
-loopCount = int(raw_input(""))
-
-for loop in range(loopCount):
-    inputText = raw_input("")
+for loop in range(input()):
+    inputText = raw_input()
     inputLengh = len(inputText)
 
-
-    outputArray = [inputLengh * '0']
+    outputArray = ['0'] * inputLengh
     oddStartIndex = 0
     if inputLengh % 2 == 0:
         oddStartIndex = inputLengh / 2
@@ -16,13 +10,9 @@ for loop in range(loopCount):
         oddStartIndex = (inputLengh + 1) / 2
 
     for i in range(len(inputText)):
-        if i * 2 < len(inputText):
-            outputArray[i] = inputText[i * 2]
+        if i % 2 == 0:
+            outputArray[i/2] = inputText[i]        
         else:
-            outputArray[i] = inputText[1 + oddStartIndex * 2]
-        oddStartIndex = oddStartIndex + 1
-    print outputArray
-
-
-    ## 길이가 홀수: 짝수의 갯수: 전체 길이 + 1 /2
-    ## 길이가 짝수: 짝수의 갯수: 전체 길이 /2
+            outputArray[oddStartIndex] = inputText[i]
+            oddStartIndex = oddStartIndex + 1    
+    print(''.join(outputArray))
---
+++ Python/Test/문자열_Test.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+age = 20
+name = 'ShinYC'
+
+print '{0} was {1} years old'.format(name, age)
+
+logname = '안녕하세요'
+print logname
